373fa5a7d315c4db1cdcb3fb4dc6392869af92b2
imjchiang <jchiang296@gmail.com>
2020-09-05 20:58:41 -0700

    [NewPost.js] fix descriptionAndCode typos, implement dotenv, comment out nonimplement code, description type to text

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -4,68 +4,69 @@ import axios from 'axios';
 const NewPostForm = (props) => {
     let [title, setTitle]= useState("")
     let [tag, setTag] = useState("")
-    let [desc, setDesc] = useState("")
+    let [descriptionAndCode, setDescriptionAndCode] = useState("")
 
     let submitForm = (e) => {
         e.preventDefault()
         // passing state variable works for key and value pair
-        // let newPost = { title, desc, tag }
-        // axios.post(`${REACT_APP_SERVER_URL}/api/post/waitiing`, newPost)
-        .then(response => response.json())
+        let newPost = { title, descriptionAndCode, tag }
+        // console.log(newPost);
+        axios.post(`${process.env.REACT_APP_SERVER_URL}/api/posts/`, newPost)
+        // .then(response => response.json())
         .then(()=> {
             setTitle("")
             setTag("")
-            setDesc("")
+            setDescriptionAndCode("")
         })
-        .then(()=>{
-            //ALL states are SET then.
-            props.refreshPosts()
-        })
+        // .then(()=>{
+        //     //ALL states are SET then.
+        //     props.refreshPosts()
+        // })
         .catch(error => console.log(error))
     }
 
-  return (
-      <div className="row mt-4">
-          <div className="col-md-7 offset-md-3">
-              <div className="card card-body">
-                  <h2>Post your question</h2>
-                  <form onSubmit={submitForm}>
-                  <div class="form-group col-md-6">
-                  <label htmlFor="title">Title</label>
-                    <input type="title" name="title" value={title} onChange={(e) => {setTitle(e.target.value)}} class="form-control" required/>
-                  </div>
-                  <div class="form-group col-md-6">
-                  <label htmlFor="desc">Description</label>
-                    <textarea type="desc" name="desc" value={desc} onChange={(e) => {setDesc(e.target.value)}} class="form-control" required/>
-                  </div>
-                  <div class="form-check form-check-inline">
-                    <input type="checkbox" name="tag-html" onChange={(e) => {setTag(e.target.checked)}} />
-                    <label class="form-check-label" htmlFor="tag-html">html</label>
-                  </div>
-                  <div class="form-check form-check-inline">
-                    <input type="checkbox" name="tag-javascript" onChange={(e) => {setTag(e.target.checked)}} />
-                    <label class="form-check-label" htmlFor="tag-javascript">javascript</label>
-                  </div>
-                  <div class="form-check form-check-inline">
-                    <input type="checkbox" name="tag-css" onChange={(e) => {setTag(e.target.checked)}} />
-                    <label class="form-check-label" htmlFor="tag-css">css</label>
-                  </div>
-                  <div class="form-check form-check-inline">
-                    <input type="checkbox" name="tag-mongodb" onChange={(e) => {setTag(e.target.checked)}} />
-                    <label class="form-check-label" htmlFor="tag-mongodb">MongoDB</label>
-                  </div>
-                  <div class="form-check form-check-inline">
-                    <input type="checkbox" name="tag-nodejs" onChange={(e) => {setTag(e.target.checked)}} />
-                    <label class="form-check-label" htmlFor="tag-nodejs">node.js</label>
-                  </div>
-                  <button type="submit" className="btn btn-primary">Submit</button>
+    return (
+        <div className="row mt-4">
+            <div className="col-md-7 offset-md-3">
+                <div className="card card-body">
+                    <h2>Post your question</h2>
+                    <form onSubmit={submitForm}>
+                    <div className="form-group col-md-6">
+                    <label htmlFor="title">Title</label>
+                        <input type="title" name="title" value={title} onChange={(e) => {setTitle(e.target.value)}} className="form-control" required/>
+                    </div>
+                    <div className="form-group col-md-6">
+                    <label htmlFor="descriptionAndCode">Description or Code</label>
+                        <textarea type="text" name="descriptionAndCode" value={descriptionAndCode} onChange={(e) => {setDescriptionAndCode(e.target.value)}} className="form-control" required/>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input type="checkbox" name="tag-html" onChange={(e) => {setTag(e.target.checked)}} />
+                        <label className="form-check-label" htmlFor="tag-html">html</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input type="checkbox" name="tag-javascript" onChange={(e) => {setTag(e.target.checked)}} />
+                        <label className="form-check-label" htmlFor="tag-javascript">javascript</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input type="checkbox" name="tag-css" onChange={(e) => {setTag(e.target.checked)}} />
+                        <label className="form-check-label" htmlFor="tag-css">css</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input type="checkbox" name="tag-mongodb" onChange={(e) => {setTag(e.target.checked)}} />
+                        <label className="form-check-label" htmlFor="tag-mongodb">MongoDB</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input type="checkbox" name="tag-nodejs" onChange={(e) => {setTag(e.target.checked)}} />
+                        <label className="form-check-label" htmlFor="tag-nodejs">node.js</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
 
-                  </form>
-              </div>
-          </div>
-      </div>
+                    </form>
+                </div>
+            </div>
+        </div>
 
-     )
+    )
   
 }
 
