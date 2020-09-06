d050c7f962a693d64e80a2adb06d09505f5746de
Irene <airine@Irenes-MacBook-Air.local>
2020-09-05 23:51:00 -0700

    [Post.js] Add specific post function

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,11 +2,19 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 
 function Post(props) {
-    
+    let post = props.location.state;
 
     return(
         <div>
             <h1>We are looking at a specific post</h1>
+            <p>{post.title}</p>
+            <p>{post.tags}</p>
+            <p>{post.descriptionsAndCode}</p>
+            <p>{post.author}</p>
+            <p>{post.upvote}</p>
+            <p>{post.downvote}</p>
+            <p>{post.solve}</p>
+            <p>{post.date}</p>
             
         </div>
     )
