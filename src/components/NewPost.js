1fdac106f3db2004f713f1c3abee0d1e501bf698
imjchiang <jchiang296@gmail.com>
2020-09-06 18:30:09 -0700

    [NewPost.js] axios call to /api/tags, create state for allTags, redo rendering for tag checkboxes

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer } from 'react' ;
+import React, { useState, useEffect } from 'react' ;
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
 
@@ -6,6 +6,19 @@ const NewPostForm = (props) => {
     let [title, setTitle]= useState("")
     let [tag, setTag] = useState("")
     let [descriptionAndCode, setDescriptionAndCode] = useState([""])
+    let [allTags, setAllTags] = useState("");
+
+    useEffect(() =>
+    {
+        axios.get(`${process.env.REACT_APP_SERVER_URL}/api/tags`)
+        .then(response =>
+        {
+            // console.log(response.data);
+            setAllTags(response.data);
+        });
+    }, []);
+
+
 
     let submitForm = (e) => {
         e.preventDefault()
@@ -27,45 +40,41 @@ const NewPostForm = (props) => {
     }
 
     return (
-        <div className="row mt-4">
-            <div className="col-md-7 offset-md-3">
-                <div className="card card-body">
-                    <h2>Post your question</h2>
-                    <form onSubmit={submitForm}>
-                        <div className="form-group col-md-6">
-                            <label htmlFor="title">Title</label>
-                                <input type="title" name="title" value={title} onChange={(e) => {setTitle(e.target.value)}} className="form-control" required/>
+        <>
+            {allTags ? 
+                <div className="row mt-4">
+                    <div className="col-md-7 offset-md-3">
+                        <div className="card card-body">
+                            <h2>Post your question</h2>
+                            <form onSubmit={submitForm}>
+                                <div className="form-group col-md-6">
+                                    <label htmlFor="title">Title</label>
+                                        <input type="title" name="title" value={title} onChange={(e) => {setTitle(e.target.value)}} className="form-control" required/>
+                                </div>
+                                <div className="form-group col-md-6">
+                                    <label htmlFor="descriptionAndCode">Description or Code</label>
+                                        <textarea type="text" name="descriptionAndCode" value={descriptionAndCode} onChange={(e) => {setDescriptionAndCode([e.target.value])}} className="form-control" required/>
+                                </div>
+                                {
+                                allTags.map((eachTag, idx) =>
+                                    {
+                                        return(
+                                            <div key={idx} className="form-check form-check-inline">
+                                                <input type="checkbox" name={"tag-" + eachTag.name} onChange={(e) => {setTag(e.target.checked)}} />
+                                                <label className="form-check-label" htmlFor={"tag-" + eachTag.name}>{eachTag.name}</label>
+                                            </div>
+                                        )
+                                    })
+                                }
+                                <button type="submit" className="btn btn-primary">Submit</button>
+                            </form>
                         </div>
-                        <div className="form-group col-md-6">
-                            <label htmlFor="descriptionAndCode">Description or Code</label>
-                                <textarea type="text" name="descriptionAndCode" value={descriptionAndCode} onChange={(e) => {setDescriptionAndCode([e.target.value])}} className="form-control" required/>
-                        </div>
-                        <div className="form-check form-check-inline">
-                            <input type="checkbox" name="tag-html" onChange={(e) => {setTag(e.target.checked)}} />
-                            <label className="form-check-label" htmlFor="tag-html">html</label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                            <input type="checkbox" name="tag-javascript" onChange={(e) => {setTag(e.target.checked)}} />
-                            <label className="form-check-label" htmlFor="tag-javascript">javascript</label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                            <input type="checkbox" name="tag-css" onChange={(e) => {setTag(e.target.checked)}} />
-                            <label className="form-check-label" htmlFor="tag-css">css</label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                            <input type="checkbox" name="tag-mongodb" onChange={(e) => {setTag(e.target.checked)}} />
-                            <label className="form-check-label" htmlFor="tag-mongodb">MongoDB</label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                            <input type="checkbox" name="tag-nodejs" onChange={(e) => {setTag(e.target.checked)}} />
-                            <label className="form-check-label" htmlFor="tag-nodejs">node.js</label>
-                        </div>
-                        <button type="submit" className="btn btn-primary">Submit</button>
-
-                    </form>
+                    </div>
                 </div>
-            </div>
-        </div>
+                :
+                <h3>Loading...</h3>
+            }
+        </>
 
     )
   
