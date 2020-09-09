ba3f3f33f27866ef9fcef491c0f9605d7a038521
imjchiang <jchiang296@gmail.com>
2020-09-09 10:05:17 -0700

    [Home.js] complete home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,20 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Home = () =>
 {
     return(
         <div>
-            <h1>Home Page</h1>
+            <h1>exitcodeZero</h1>
+            <p>To ask questions on coding, debugging, and the theory behind coding and to help others in the community of coders that collectively ask, learn, and help.</p>
+            <p>Create an account with us to have access to even more features...</p>
+            <li>Ask Questions</li>
+            <li>Create Solutions</li>
+            <li>Promote Good Content</li>
+            <h3>Explore Questions Now!!</h3>
+            <Link to="/allPosts">
+                <button>Let's Go</button>
+            </Link>
         </div>
     )
 }
