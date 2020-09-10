6d50b677c75465b4f81a08308bf601924b78aa22
imjchiang <jchiang296@gmail.com>
2020-09-10 14:34:02 -0700

    [AllPosts.js] implement tag filter toggle, add styling [AllPosts.css] implement styling for AllPosts page [Home.css] change some styling

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import {Link} from "react-router-dom";
+import "../css/AllPosts.css"
 import axios from "axios";
 
 const Feed = (props) =>
@@ -7,6 +8,7 @@ const Feed = (props) =>
     // console.log(props);
     const [posts, setPosts] = useState([]);
     const [tags, setTags] = useState([])
+    const [filter, setFilter] = useState(false)
     let [allTags, setAllTags] = useState([]);
 
     useEffect(() => 
@@ -34,8 +36,8 @@ const Feed = (props) =>
     const newPost = 
     (
         <div>
-            <Link to="/post/new">
-                <button>Ask a Question</button>
+            <Link className="options" to="/post/new">
+                Ask a Question
             </Link>
         </div>
     )
@@ -45,88 +47,130 @@ const Feed = (props) =>
     {
         return(
             <div>
-                <Link to="/login">
-                    <button>Login to Ask a Question</button>
+                <Link className="options" to="/login">
+                    Login to Ask a Question
                 </Link>
             </div>
         );
     };
 
+    const toggleFilter = () =>
+    {
+        let currentFilter = filter;
+        currentFilter = !currentFilter
+        setFilter(currentFilter);
+    }
+
     return(
         <div>
-            <button>Tags filter</button>
-            <br/>
-            {allTags.map((eachTag, idx) =>
-            {
-                return(
-                    <div key={idx} className="form-check form-check-inline">
-                        <input type="checkbox" name={"tag-" + eachTag.name} value={eachTag._id} onChange={(e) => {
-                            if(e.target.checked) 
-                            {
-                                if (tags.length >= 5) {
-                                    alert("Max 5 tags!")
-                                    e.preventDefault()
-                                    return
-                                }
-                                let newTags = []
-                                newTags = newTags.concat(tags,[e.target.value] )
-                                console.log(newTags)
-                                setTags(newTags) 
-                            } 
-                            else 
-                            {
-                                let newTags = []
-                                newTags = tags.filter((t)=>{
-                                    return t !== e.target.value
-
-                                })
-                                setTags(newTags)
-                            }
-                                    }} />
-                        <label className="form-check-label" htmlFor={"tag-" + eachTag.name}>{eachTag.name}</label>
-                    </div>
-                )
-            })} 
-            <button onClick={()=>{
-                let filter = tags.join(',')
-                let url = `${process.env.REACT_APP_SERVER_URL}/api/posts/?filter=${filter}`
-                console.log(url)
-                axios.get(`${process.env.REACT_APP_SERVER_URL}/api/posts/?filter=${filter}`)
-                .then(response => 
-                {
-                    console.log(response.data);
-                    setPosts(response.data);
-                });
-            }} type="submit" className="btn btn-primary">Submit</button>
-
-            <h1>Posts and Questions</h1>
-            {props.user ? newPost : errorDiv()}
-            <hr />
-            <div>
-            {posts.map((post, idx) => 
-            {
-                let location = 
-                {
-                    pathname: `/post`,
-                    state: post
-                }
-                // `/post/${post._id}`
-                return (
-                    <div key={idx}>
-                        <Link to={location} key={post._id}>
-                            {post.title}
-                        </Link>
-                        <br/>
-                        {post.author ? post.author.name : "No author available"}
-                        <br/>
-                        {post.descriptionAndCode}
-                        <br/>
-                        <hr />
-                    </div>
-                )
-            }) }
+            <h2>- Posts and Questions -</h2>
+            <div className="allposts-options">
+                {props.user ? newPost : errorDiv()}
+                <button onClick={toggleFilter} className="options">Tags filter</button>
             </div>
-            <p>YEET</p>
+            <hr />
+
+            {filter 
+            ?
+                <> 
+                    <h6 className="allposts-filter">Filter by Tag: </h6>
+                    <br />
+                    {allTags.map((eachTag, idx) =>
+                    {
+                        return(
+                            <div key={idx} className="form-check form-check-inline">
+                                <input type="checkbox" 
+                                    name={"tag-" + eachTag.name} 
+                                    value={eachTag._id} 
+                                    onChange={(e) => 
+                                    {
+                                        if(e.target.checked) 
+                                        {
+                                            if (tags.length >= 5) 
+                                            {
+                                                alert("Max 5 tags!")
+                                                e.preventDefault()
+                                                return
+                                            }
+                                            let newTags = []
+                                            newTags = newTags.concat(tags,[e.target.value])
+                                            console.log(newTags)
+                                            setTags(newTags) 
+                                        } 
+                                        else 
+                                        {
+                                            let newTags = []
+                                            newTags = tags.filter((t)=>
+                                            {
+                                                return t !== e.target.value
+                                            })
+                                            setTags(newTags)
+                                        }
+                                    }} 
+                                />
+                                <label className="form-check-label" htmlFor={"tag-" + eachTag.name}>{eachTag.name}</label>
+                            </div>
+                        )
+                    })} 
+                    <button type="submit" 
+                        className="btn btn-primary"
+                        onClick={() => 
+                        {
+                            let filter = tags.join(',')
+                            let url = `${process.env.REACT_APP_SERVER_URL}/api/posts/?filter=${filter}`
+                            console.log(url)
+                            axios.get(`${process.env.REACT_APP_SERVER_URL}/api/posts/?filter=${filter}`)
+                            .then(response => 
+                            {
+                                console.log(response.data);
+                                setPosts(response.data);
+                            });
+                        }}
+                    >
+                        Submit
+                    </button>
+                    <hr />
+                </>
+            :
+                console.log("NO FILTER")
+            }
+
+            <div>
+                {posts.map((post, idx) => 
+                {
+                    let location = 
+                    {
+                        pathname: `/post`,
+                        state: post
+                    }
+                    return (
+                        <div key={idx}>
+                            <Link className="allposts-title" to={location} key={post._id}>
+                                {post.title.length > 50 
+                                ? 
+                                    post.title.substring(0, 50) + " . . ."
+                                :
+                                    post.title
+                                }
+                            </Link>
+                            <br/>
+                            <div className="allposts-desc">
+                                {post.descriptionAndCode[0].length > 250
+                                ?
+                                    post.descriptionAndCode[0].substring(0, 250) + " . . ."
+                                :
+                                    post.descriptionAndCode
+                                }
+                            </div>
+                            <div className="allposts-author">
+                                {post.author ? "Posted by " + post.author.name : "No author available"}
+                            </div>
+                            <hr />
+                        </div>
+                    )
+                })}
+            </div>
+            <br />
             <br />
             <br />
             <br />
